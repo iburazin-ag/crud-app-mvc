d49122409aeb9c355b0655fa5e76db4d41fca492
iburazin-ag <ivana.burazin@agilathon.com>
2022-12-21 10:38:37 +0100

    cleanup app.js, remove routes

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express")
-
 const app = express()
+
+const router = require('./router')
 
 app.use(express.static('public'))
 app.set('views', 'views')
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.render('home-guest')
-})
+app.use('/', router)
 
 app.listen(3000, () => {
     console.log(`Listening on port 3000`)
