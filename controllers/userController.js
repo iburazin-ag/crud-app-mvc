fa7d86e8686594cb7bb5e89bb0205df52d9cc297
iburazin-ag <ivana.burazin@agilathon.com>
2022-12-22 10:21:38 +0100

    login issue fix, reverted to course provided code

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,28 +1,28 @@
 const User = require('../models/User')
 
-exports.login = (req, res) => {
-    const user = new User(req.body)
-    user.login()
-    res.send('successful login')
+exports.login = function(req, res) {
+  let user = new User(req.body)
+  user.login().then(function(result) {
+    res.send(result)
+  }).catch(function(e) {
+    res.send(e)
+  })
 }
 
-exports.logout = (req, res) => {
-    
+exports.logout = function() {
+  
 }
 
-exports.register = (req, res) => {
-    //const { username, email, password } = req.body
-    //const user = new User({ username, email, password })
-    const user = new User(req.body)
-    user.register()
-    if (user.errors.length) {
-        res.send(user.errors)
-    } else {
-        res.send("No errors")
-    }
+exports.register = function(req, res) {
+  let user = new User(req.body)
+  user.register()
+  if (user.errors.length) {
+    res.send(user.errors)
+  } else {
+    res.send("Congrats, there are no errors.")
+  }
 }
 
-
-exports.home = (req, res) => {
-    res.render('home-guest')
+exports.home = function(req, res) {
+  res.render('home-guest')
 }
